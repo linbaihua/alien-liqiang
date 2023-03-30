6b6b5c9787e548b8478a29eea229f2aba1a957a2
linbaihua <1715991019@qq.com>
2023-03-30 23:02:53 +0800

    Please enter the commit message for your changes. Lines startingith '#' will be ignored, and an empty message aborts the commit.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import { Button, Select, Row } from 'element-ui';
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Row)
 
 new Vue({
   render: h => h(App),
