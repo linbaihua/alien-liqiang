cbdd3747658d82429aa30eee1e279e95ca8b2632
linbaihua <1715991019@qq.com>
2023-03-30 23:38:30 +0800

    config the vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import { Button, Select, Row } from 'element-ui';
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -9,5 +10,6 @@ Vue.use(Select)
 Vue.use(Row)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 0. 如果使用模块化机制编程，导入Vue和VueRouter，要调用 Vue.use(VueRouter)
+
+// 1. 定义 (路由) 组件。
+// 可以从其他文件 import 进来
+import Home from '../views/Home.vue'
+import User from '../views/User.vue'
+
+// 2. 定义路由
+// 每个路由应该映射一个组件。 其中"component" 可以是
+// 我们晚点再讨论嵌套路由。
+const routes = [
+  { path: '/home', component: Home },
+  { path: '/user', component: User }
+]
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
+
+// 4. 对外暴露 router 实例
+export default router
+
+// 5. 将router挂载到根实例中
